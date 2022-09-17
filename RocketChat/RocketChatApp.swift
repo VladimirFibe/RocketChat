013cb9e61061b2a2fa94105d9a51d706717213ca
Vladimir <motiw@icloud.com>
2022-09-17 14:08:03 +0600

    Initial Commit

---
+++ RocketChat/RocketChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketChatApp.swift
+//  RocketChat
+//
+//  Created by Vladimir Fibe on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RocketChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
